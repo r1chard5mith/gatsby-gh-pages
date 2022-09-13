1f3daa4eda68f28420787a3298455542206136f6
Richard Smith <r1chard5mith@icloud.com>
2022-09-13 16:49:31 +1000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,10 @@ const IndexPage = () => {
     <main>
       <h1>Welcome to my Gatsby site!!!</h1>
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <ul>
+        <li> Thing one        </li>
+        <li> Thing Two </li>
+      </ul>
     </main>
   )
 }
